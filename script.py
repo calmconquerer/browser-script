3cbe7cb991d4e649f32e877902a346598ecb500a
calmconquerer <wasiullah.khan21@gmail.com>
2019-03-21 17:44:37 +0500

    Added documentation and a configration file

--- script.py
+++ script.py
@@ -1,13 +1,11 @@
 import webbrowser as wb
+import config
 
-backing_tracks = {
-    'track1': 'https://www.youtube.com/watch?v=BW73rDPVAU0&list=RDMMBW73rDPVAU0&index=1',
-    'track2': 'https://www.youtube.com/watch?v=VyhUrgIRn7g&list=RDMMBW73rDPVAU0&index=2',
-    'track3': 'https://www.youtube.com/watch?v=-HraMOLxPyE&list=RDMMBW73rDPVAU0&index=9',
-    'track4': 'https://www.youtube.com/watch?v=I7LmHILR9AQ',
-    'track5': 'https://www.youtube.com/watch?v=EHnuopwhgB0',
-    'track6': 'https://www.youtube.com/watch?v=6Rf_7SVJJNM'
-}
 
-for key in backing_tracks:
-    wb.open(backing_tracks[key])
+'''
+    A python script which, after execution, opens up the links I have saved in the browser.
+    You can change or add new links in the config file
+'''
+
+for key in config.backing_tracks:
+    wb.open(config.backing_tracks[key])
