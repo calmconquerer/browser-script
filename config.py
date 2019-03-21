2b8c7a6cc5d8efd75629902a796cc5f8c8b4036f
calmconquerer <wasiullah.khan21@gmail.com>
2019-03-21 17:43:27 +0500

    Added documentation and a configuration file

---
+++ config.py
@@ -0,0 +1,8 @@
+backing_tracks = {
+    'link1': 'https://www.youtube.com/watch?v=BW73rDPVAU0&list=RDMMBW73rDPVAU0&index=1',
+    'link2': 'https://www.youtube.com/watch?v=VyhUrgIRn7g&list=RDMMBW73rDPVAU0&index=2',
+    'link3': 'https://www.youtube.com/watch?v=-HraMOLxPyE&list=RDMMBW73rDPVAU0&index=9',
+    'link4': 'https://www.youtube.com/watch?v=I7LmHILR9AQ',
+    'link5': 'https://www.youtube.com/watch?v=EHnuopwhgB0',
+    'link6': 'https://www.youtube.com/watch?v=6Rf_7SVJJNM'
+}
